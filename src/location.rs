20e794f19bee302df6f25d87114a36ad97ff1303
Garrett Kinman <garrettkinman@gmail.com>
2021-06-21 23:52:11 -0400

    fixed module for location #3

--- src/location.rs
+++ src/location.rs
@@ -2,53 +2,51 @@ mod economy;
 mod government;
 mod trade;
 
-mod location {
-    pub trait Location {
-        // TODO
-    }
+pub trait Location {
+    // TODO
+}
+
+pub struct Faction {
+    id: u32,
+    name: String,
+    population: u64,
     
-    pub struct Faction {
-        id: u32,
-        name: String,
-        population: u64,
-        
-        econ: economy::Economy<Faction>,
-        gov: government::Government,
+    econ: economy::Economy<Faction>,
+    gov: government::Government,
 
-        capital: City,
-        
-        rivals: Vec<Faction>,
-        allies: Vec<Faction>,
-        tradePartners: Vec<Faction>,
-
-        regions: Vec<Region>,
-        cities: Vec<City>,
-    }
+    capital: City,
     
-    pub struct Region {
-        id: u32,
-        name: String,
-        population: u64,
-        
-        econ: Economy<Region>,
+    rivals: Vec<Faction>,
+    allies: Vec<Faction>,
+    tradePartners: Vec<Faction>,
 
-        faction: Faction,
-        capital: City,
+    regions: Vec<Region>,
+    cities: Vec<City>,
+}
 
-        cities: Vec<City>,
-    }
+pub struct Region {
+    id: u32,
+    name: String,
+    population: u64,
     
-    pub struct City {
-        id: u32,
-        name: String,
-        population: u64,
+    econ: Economy<Region>,
 
-        econ: Economy<City>,
+    faction: Faction,
+    capital: City,
 
-        faction: Faction,
-        region: Region,
+    cities: Vec<City>,
+}
 
-        markets: Vec<trade::TradeExchange>,
+pub struct City {
+    id: u32,
+    name: String,
+    population: u64,
 
-    }
+    econ: Economy<City>,
+
+    faction: Faction,
+    region: Region,
+
+    markets: Vec<trade::TradeExchange>,
+
 }
